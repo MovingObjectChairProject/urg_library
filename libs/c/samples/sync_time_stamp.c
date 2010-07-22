abcee1aa8c8245c6098d3872d32339d48c52d0b8
Satofumi KAMIMURA <Satofumi KAMIMURA@localhost>
2010-07-23 08:43:21 +0900

    removed debug message

---
+++ libs/c/samples/sync_time_stamp.c
@@ -0,0 +1,52 @@
+/*!
+  \example sync_time_stamp.c センサと PC のタイムスタンプを同期する
+
+  \author Satofumi KAMIMURA
+
+  $Id$
+
+  \todo Windows の場合に COMx が選択されるようにする
+*/
+
+#include "urg_sensor.h"
+#include "urg_utils.h"
+#include <stdio.h>
+
+
+int main(void)
+{
+    enum {
+        TIME_STAMP_RECEIVE_TIMES = 5,
+    };
+
+    urg_t urg;
+    long time_stamp;
+    int i;
+
+    // !!!
+
+    if (urg_open(&urg, URG_SERIAL, "/dev/ttyACM0", 115200) < 0) {
+        printf("urg_open: %s\n", urg_error(&urg));
+        return 1;
+    }
+
+    // !!!
+
+    // !!!
+    urg_start_time_stamp_mode(&urg);
+    for (i = 0; i < TIME_STAMP_RECEIVE_TIMES; ++i) {
+        time_stamp = urg_time_stamp(&urg);
+        if (time_stamp < 0) {
+            // !!! error_handling
+            // !!!
+        }
+        // !!!
+    }
+    urg_stop_time_stamp_mode(&urg);
+
+    // !!! URG のタイムスタンプと PC の時間の関係を出力する
+
+    urg_close(&urg);
+
+    return 0;
+}
---
+++ libs/c/urg_serial.h
@@ -0,0 +1,59 @@
+#ifndef URG_SERIAL_H
+#define URG_SERIAL_H
+
+/*!
+  \file
+  \brief シリアル通信
+
+  \author Satofumi KAMIMURA
+
+  $Id$
+*/
+
+#include "urg_detect_os.h"
+
+#if defined(URG_WINDOWS_OS)
+#include <windows.h>
+#else
+#include <termios.h>
+#endif
+#include "urg_ring_buffer.h"
+
+
+enum {
+    RING_BUFFER_SIZE_SHIFT = 7,
+    RING_BUFFER_SIZE = 1 << RING_BUFFER_SIZE_SHIFT,
+
+    ERROR_MESSAGE_SIZE = 256,
+};
+
+
+typedef struct
+{
+#if defined(URG_WINDOWS_OS)
+    HANDLE hCom;                /*!< 接続リソース */
+    int current_timeout;        /*!< タイムアウトの設定時間 [msec] */
+#else
+    int fd;
+    struct termios sio;
+#endif
+
+    ring_buffer_t ring;         /*!< リングバッファ */
+    char buffer[RING_BUFFER_SIZE];
+    char has_last_ch;          /*!< 書き戻した文字があるかのフラグ */
+    char last_ch;              /*!< 書き戻した１文字 */
+    char error_string[ERROR_MESSAGE_SIZE];
+} serial_t;
+
+
+extern int serial_open(serial_t *serial, const char *device, long baudrate);
+extern void serial_close(serial_t *serial);
+extern int serial_set_baudrate(serial_t *serial, long baudrate);
+extern int serial_write(serial_t *serial, const char *data, int size);
+extern int serial_read(serial_t *serial,
+                       char *data, int max_size, int timeout);
+extern int serial_readline(serial_t *serial,
+                           char *data, int max_size, int timeout);
+extern int serial_error(serial_t *serial, char *error_message, int max_size);
+
+#endif /* !URG_SERIAL_H */
