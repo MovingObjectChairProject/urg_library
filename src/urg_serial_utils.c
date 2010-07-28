b7cbb8e6570b86ff79d25c1c5d5ba8825fcf9a41
Satofumi KAMIMURA <Satofumi KAMIMURA@localhost>
2010-07-29 04:06:53 +0900

    fixed error handling

---
+++ src/urg_serial_utils.c
@@ -0,0 +1,17 @@
+/*!
+  \file
+  \brief シリアル用の補助関数
+
+  \author Satofumi KAMIMURA
+
+  $Id$
+*/
+
+#include "urg_serial_utils.h"
+
+
+#if 0
+#include "urg_serial_utils_windows.c"
+#else
+#include "urg_serial_utils_linux.c"
+#endif
